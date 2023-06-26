990b75d55301effa7fa623a9a59d2b18ffed1f9d
Cami <lexaissupreme@gmail.com>
2023-06-26 15:28:08 -0400

    Initial Commit

---
+++ itswhatever/itswhateverApp.swift
@@ -0,0 +1,17 @@
+//
+//  itswhateverApp.swift
+//  itswhatever
+//
+//  Created by scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct itswhateverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
